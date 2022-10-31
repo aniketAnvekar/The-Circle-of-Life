b52118bb428c5704c4ca594c5bb2ad956934d619
Shivam Agrawal <agrawalshivam1119@gmail.com>
2022-10-31 17:29:53 -0400

    Making Agent 1

--- Graph_01.py
+++ Graph_01.py
@@ -4,9 +4,10 @@ import matplotlib.pyplot as plt
 
 class Graph:
 
-    def __init__(self, nodes):
+    def __init__(self, nodes, config):
         self.alist = {}
         self.nodes = nodes
+        self.config = config
 
     def create(self):
         for i in range(self.nodes-1):
@@ -20,7 +21,7 @@ class Graph:
         self.alist[self.nodes-1].append(self.nodes-2)
         self.alist[0].append(self.nodes-1)
         self.alist = self.create_additionalEdges()
-        print(self.alist)
+        # print(self.alist)
         return self.alist
 
     # def getElement(self, arr):
@@ -63,7 +64,7 @@ class Graph:
                 else:
                     random_choice_index = random.randrange(valid_neighbor_count)
                     random_choice = valid_neighbors[random_choice_index]
-                    print(j, i, random_choice)
+                    # print(j, i, random_choice)
                     j = j + 1
                     self.alist[i].append(random_choice)
                     self.alist[random_choice].append(i)
@@ -87,46 +88,9 @@ class Graph:
     def visualize(self):
         G = nx.Graph()
         
-        for i in range(0, 50):
+        for i in range(0, self.config["GRAPH_SIZE"]):
             G.add_node(i) 
         
         edges = self.create_edges()
         G.add_edges_from(edges)
         return G
-
-
-
-
-       
-
-                # flag = True
-                # while flag:
-                    
-
-
-
-                    # move_decider = random.randint(1,2)
-                    # if(move_decider == 1):
-                    #     print('Move forward')
-                    #     arr = [k%self.nodes for k in range(i+2,i+6)]
-                    #     ele = self.getElement(arr)
-                    #     if ele and ele!=-1:
-                    #         self.alist[i].append(ele)
-                    #         self.alist[ele].append(i)
-                    #         print(self.alist[i], self.alist[ele])
-                    #         flag = False
-                    #         break
-                    # else:
-                    #     print('Move backward') 
-                    #     node_list = []
-                    #     for k in range(5,1,-1):
-                    #         node_list.append((self.nodes+(i-k))%self.nodes)
-                    # #    print(i)
-                    #     print(node_list)
-                    #     ele = self.getElement(node_list)
-                    #     if ele and ele!=-1:
-                    #         self.alist[i].append(ele)
-                    #         self.alist[ele].append(i)
-                    #         print(self.alist[i], self.alist[ele])
-                    #         flag = False
-                    #         break
